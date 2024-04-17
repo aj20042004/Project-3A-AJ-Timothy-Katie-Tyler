c421a92b97d108d4f07ff26b0323afe36afea9df
KatieD4 <162634216+KatieD4@users.noreply.github.com>
2024-04-17 12:30:22 -0500

    Tree_Node class.
    Tree_Node header and cpp file...

---
+++ Binary Tree Expression Parser/Tree_Node.cpp
@@ -0,0 +1,6 @@
+#include "Tree_Node.h"
+
+using namespace std;
+
+// Constructor
+Tree_Node::Tree_Node(const Token& token) : data(token), left(nullptr), right(nullptr) {}
--- Binary Tree Expression Parser/Tree_Node.h
+++ Binary Tree Expression Parser/Tree_Node.h
@@ -1,10 +1,15 @@
 #ifndef TREE_NODE_H
 #define TREE_NODE_H
+#include"Token.h"
 
+struct Tree_Node {
+	// Data fields.
+	Token data;
+	Tree_Node* left, right;
 
+	// Constructor.
+	Tree_Node(const Token& token);
 
-
-
-
+};
 
 #endif
